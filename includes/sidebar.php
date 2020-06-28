1a81868bc5e051d587e6372aee64544f1b721449
Bhawishya-khanal <62171047+Bhawishya-khanal@users.noreply.github.com>
2020-06-28 11:14:27 +0545

    Add files via upload

---
+++ includes/sidebar.php
@@ -0,0 +1,39 @@
+
+
+<div class="row">
+	<div class="box box-solid">
+	  	<div class="box-header with-border">
+	    	<h3 class="box-title"><b>Become a Subscriber</b></h3>
+	  	</div>
+	  	<div class="box-body">
+	    	<p>Get free updates on the latest products and discounts, straight to your inbox.</p>
+	    	<form method="POST" action="">
+		    	<div class="input-group">
+	                <input type="text" class="form-control">
+	                <span class="input-group-btn">
+	                    <button type="button" class="btn btn-info btn-flat"><i class="fa fa-envelope"></i> </button>
+	                </span>
+	            </div>
+		    </form>
+	  	</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class='box box-solid'>
+	  	<div class='box-header with-border'>
+	    	<h3 class='box-title'><b>Follow us on Social Media</b></h3>
+	  	</div>
+	  	<div class='box-body'>
+	    	<a href="https://www.facebook.com/goodluckhandicraftnepal/" target="_blank" class="btn btn-social-icon btn-facebook"><i class="fa fa-facebook"></i></a>	    	
+	    	<a href="https://www.instagram.com/goodluckhandicrafts/" target="_blank" class="btn btn-social-icon btn-instagram"><i class="fa fa-instagram"></i></a>
+	  	</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class='box box-solid'>
+		<p><b>Our physical location</b></p>
+	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d303.30288951065603!2d85.31153516041064!3d27.71634432467675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5f96c295e28eb9fc!2sGoodluck%20Handicrafts!5e0!3m2!1sen!2snp!4v1574695280454!5m2!1sen!2snp" width="285" height="200" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+	</div>
+</div>
